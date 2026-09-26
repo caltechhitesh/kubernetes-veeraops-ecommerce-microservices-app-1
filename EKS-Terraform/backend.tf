dae0ce7c492420a49c55d75b0c852990a986ffdc
Hitesh Valkundakar <140967674+caltechhitesh@users.noreply.github.com>
2026-09-26 12:02:11 +0530

    Update S3 bucket name in backend configuration

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hitesh-demo-1-2026"   # <-- Wahi same S3 bucket name jo Step 1 me banaya
+    bucket = "hitesh-2026-demo"   # <-- Wahi same S3 bucket name jo Step 1 me banaya
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
   }
